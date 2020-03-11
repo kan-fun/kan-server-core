1b40ab59234f88267c8bbb3b983f63582d934c4c
Your Name <you@example.com>
2020-03-11 19:40:38 +0800

    Add Tag

--- user/user.go
+++ user/user.go
@@ -6,9 +6,9 @@ type User struct {
 	ID        uint `gorm:"primary_key"`
 	CreatedAt time.Time
 
-	Email string
-	Password string
+	Email string `gorm:"type:varchar(100);unique_index;not null"`
+	Password string `gorm:"not null"`
 
-	AccessKey string
-	SecretKey string
+	AccessKey string `gorm:"size:44;unique;not null"`
+	SecretKey string `gorm:"size:44;not null"`
 }
