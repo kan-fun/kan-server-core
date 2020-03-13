ca7b0ad7350e1b21ddebbf0ebfb76c965cb78494
Your Name <you@example.com>
2020-03-13 12:04:04 +0800

    Rename Email to ChannelEmail

---
+++ model/channel_email.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type ChannelEmail struct {
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+
+	UserID uint
+	User   User
+	Address string `gorm:"type:varchar(100);unique_index;not null"`
+	Count uint `gorm:"type:not null"`
+}
