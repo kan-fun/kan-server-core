cdab4f54fd235d71947b235d155ff678939bcc84
Your Name <you@example.com>
2020-03-19 16:32:04 +0800

    fmt

--- model/channel_email.go
+++ model/channel_email.go
@@ -6,8 +6,8 @@ type ChannelEmail struct {
 	ID        uint `gorm:"primary_key"`
 	CreatedAt time.Time
 
-	UserID uint
-	User   User
+	UserID  uint
+	User    User
 	Address string `gorm:"type:varchar(100);unique_index;not null"`
-	Count uint
+	Count   uint
 }
--- model/user.go
+++ model/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        uint `gorm:"primary_key"`
 	CreatedAt time.Time
 
-	Email string `gorm:"type:varchar(100);unique_index;not null"`
+	Email    string `gorm:"type:varchar(100);unique_index;not null"`
 	Password string `gorm:"not null"`
 
 	AccessKey string `gorm:"size:44;unique;not null"`
